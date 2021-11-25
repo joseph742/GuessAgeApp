80d54570f247c652882f827836c52fcb0e409296
joseph742 <umoru.j@gmail.com>
2021-11-25 15:09:16 +0100

    first commit

---
+++ GuessAgeApp/GuessAgeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessAgeAppApp.swift
+//  GuessAgeApp
+//
+//  Created by Qasim Ahmed on 25/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessAgeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
